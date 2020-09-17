d3f4abe71b645ca7f2562b8f9841ee6b733e6260
Falon Landers <falonlanders@outlook.com>
2020-09-17 18:52:09 -0400

    skeleton html/css

--- src/components/page/Footer/Categories.js
+++ src/components/page/Footer/Categories.js
@@ -4,9 +4,16 @@ import './Categories.css';
 
 export const Categories = () => {
 
-return (
-    <div className='categoryContainer'>
-        <p>Categories</p>
-    </div>
-)
+    return (
+        <div className='category'>
+            <h1>Categories</h1>
+
+            <container className='productCategories'>
+                <button className="cats">Working From Home</button>
+                <button className="cats">Learning From Home</button>
+                <button className="cats">Entertainment</button>
+                <button className="cats">Supplies</button>
+            </container>
+        </div>
+    )
 }
--- src/components/page/body/MainBody.js
+++ src/components/page/body/MainBody.js
@@ -4,9 +4,23 @@ import './MainBody.css';
 
 export const MainBody = () => {
 
-return (
-    <div className='categoryContainer'>
-        <p>MainBody</p>
-    </div>
-)
+    return (
+        <div className='categoryContainer'>
+            <h1>MainBody</h1>
+            <container className="productContainer">
+                <div className="product">PRODUCT</div>
+                <div className="product">PRODUCT</div>
+                <div className="product">PRODUCT</div>
+                <div className="product">PRODUCT</div>
+                <div className="product">PRODUCT</div>
+                <div className="product">PRODUCT</div>
+                <div className="product">PRODUCT</div>
+                <div className="product">PRODUCT</div>
+                <div className="product">PRODUCT</div>
+                <div className="product">PRODUCT</div>
+                <div className="product">PRODUCT</div>
+                <div className="product">PRODUCT</div>
+            </container>
+        </div>
+    )
 }
