51a49910b01b029a134bd04d80878d38f2c9825d
Nicolas Olivares <nolivaresr@hotmail.com>
2020-09-18 17:54:15 -0400

    front end file system and skeleton

---
+++ src/components/pages/body/orders/Cart.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from "react";
+
+import "./Cart.css";
+
+
+export const Cart = () => {
+    return (
+        <div className="cart">
+            <p>Cart</p>
+        </div>
+    );
+};
---
+++ src/components/pages/body/orders/Orders.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from "react";
+
+import "./Orders.css";
+
+
+export const Orders = () => {
+    return (
+        <div className="orders">
+            <p>Orders</p>
+        </div>
+    );
+};
