7ac1dc1417e8b29e5ccef3ae770524c70501f7b3
Loucifer88 <lfletcher08@gmail.com>
2020-09-19 11:09:56 -0400

    added update users

--- db/seed.js
+++ db/seed.js
@@ -1,98 +1,112 @@
-const { 
-    addUser,
-    getAllUsers
-} = require('./singletables/users');
-
+const { addUser, getAllUsers, updateUser } = require("./singletables/users");
 
 async function seed() {
-	//creating a new user
-	try {
-		console.log('creating user one');
-		const user1 = await addUser({
-			firstName: 'Nicolas',
-			lastName: 'Olivares',
-            isAdmin: true,
-            isUser: true,
-			email: 'myemail@you.com',
-			password: 'mypassword',
-            addressLine1: '4545 street',
-            addressLine2: '',
-			city: 'Jax',
-			state: 'Fl',
-			zipcode: '32210',
-			country: 'USA',
-			phone: '555-555-5555',
-            creditCard: 45454545454545455,
-            
-		});
-        console.log('this is user 1 ', user1);
-        
-        console.log('creating user two, login in with minimum info ');
-        const user2 = await addUser({
-            firstName: 'Joe',
-            lastName: 'Moe',
-            isAdmin: null,
-            isUser: null,
-			email: 'myemail2@you.com',
-			password: 'password',
-            addressLine1: null,
-            addressLine2: null,
-			city: null,
-			state: null,
-			zipcode: null,
-			country: null,
-			phone: null,
-            creditCard: null,
-        });
-        console.log('user2 with minimum data ', user2);
+  //creating a new user
+  try {
+    console.log("creating user one");
+    const user1 = await addUser({
+      firstName: "Nicolas",
+      lastName: "Olivares",
+      isAdmin: true,
+      isUser: true,
+      email: "myemail@you.com",
+      password: "mypassword",
+      addressLine1: "4545 street",
+      addressLine2: "",
+      city: "Jax",
+      state: "Fl",
+      zipcode: "32210",
+      country: "USA",
+      phone: "555-555-5555",
+      creditCard: 45454545454545455,
+    });
+    console.log("this is user 1 ", user1);
 
-        console.log('creating user three, login in with minimum info and repeated email ');
-        const user3 = await addUser({
-            firstName: 'Joe',
-            lastName: 'Moe',
-            isAdmin: null,
-            isUser: null,
-			email: 'myemail2@you.com',
-			password: 'password',
-            addressLine1: null,
-            addressLine2: null,
-			city: null,
-			state: null,
-			zipcode: null,
-			country: null,
-			phone: null,
-            creditCard: null,
-        });
-        console.log('user3 with minimum data ', user3);
+    console.log("creating user two, login in with minimum info ");
+    const user2 = await addUser({
+      firstName: "Joe",
+      lastName: "Moe",
+      isAdmin: null,
+      isUser: null,
+      email: "myemail2@you.com",
+      password: "password",
+      addressLine1: null,
+      addressLine2: null,
+      city: null,
+      state: null,
+      zipcode: null,
+      country: null,
+      phone: null,
+      creditCard: null,
+    });
+    console.log("user2 with minimum data ", user2);
 
+    console.log(
+      "creating user three, login in with minimum info and repeated email "
+    );
+    const user3 = await addUser({
+      firstName: "Joe",
+      lastName: "Moe",
+      isAdmin: null,
+      isUser: null,
+      email: "myemail2@you.com",
+      password: "password",
+      addressLine1: null,
+      addressLine2: null,
+      city: null,
+      state: null,
+      zipcode: null,
+      country: null,
+      phone: null,
+      creditCard: null,
+    });
+    console.log("user3 with minimum data ", user3);
 
-        
-        console.log('creating user four, missing info ');
-        const user4 = await addUser({
-            firstName: 'Joe',
-            lastName: 'Moe',
-            isAdmin: null,
-            isUser: null,
-			email: null,
-			password: 'password',
-            addressLine1: null,
-            addressLine2: null,
-			city: null,
-			state: null,
-			zipcode: null,
-			country: null,
-			phone: null,
-            creditCard: null,
-        });
-        console.log('user4 with minimum data ', user4);
+    console.log("creating user four, missing info ");
+    const user4 = await addUser({
+      firstName: "Joe",
+      lastName: "Moe",
+      isAdmin: null,
+      isUser: null,
+      email: null,
+      password: "password",
+      addressLine1: null,
+      addressLine2: null,
+      city: null,
+      state: null,
+      zipcode: null,
+      country: null,
+      phone: null,
+      creditCard: null,
+    });
+    console.log("user4 with minimum data ", user4);
 
-        console.log('Running getAllUsers...');
-        const allUsers = await getAllUsers();
-        console.log('all users result: ', allUsers);
+    console.log("Running getAllUsers...");
+    const allUsers = await getAllUsers();
+    console.log("all users result: ", allUsers);
 
-	} catch (error) {
-		throw error;
-	}
+    console.log("Updating User 1...");
+    const user5 = await updateUser({
+      id: 2,
+      isAdmin: false,
+      isUser: true,
+      email: "myemail2@you.com",
+      password: "password",
+      firstName: "Joe",
+      lastName: "Moe",
+      addressLine1: "1234 Anywhere",
+      addressLine2: null,
+      city: "Anywhere",
+      state: "Florida",
+      zipcode: "12345",
+      country: null,
+      phone: "123-456-7890",
+      creditCard: 1234567891234567,
+    });
+    console.log("Updated User2", user5);
+  } catch (error) {
+    throw error;
+  }
 }
 
 module.exports = { seed };
--- db/singletables/users.js
+++ db/singletables/users.js
@@ -1,97 +1,145 @@
-const { client } = require('../client');
+const { client } = require("../client");
 
-const bcrypt = require('bcrypt');
-
+const bcrypt = require("bcrypt");
 
 /**
  * Create new user by registration (first, last, email, pass minimum),
  * by guest purchase (all fields except for password),
  * @param {Object} {requires firstName, lastName, email}
- * returns a single object with { id, firstName, lastName, isAdmin, isUser, email, password, 
+ * returns a single object with { id, firstName, lastName, isAdmin, isUser, email, password,
  * addressLine1, addressLine2, city, state, zipcode, country, phone, creditcard } or
- * an object with an error message { message: 'the error' } 
+ * an object with an error message { message: 'the error' }
  */
 async function addUser({
-	firstName,
-	lastName,
-	isAdmin = false,
-	isUser = false,
-	email = null,
-	password = null,
-	addressLine1 = '',
-	addressLine2 = '',
-	city = '',
-	state = '',
-	zipcode = '',
-	country = '',
-	phone = null,
-	creditCard = null,
-	
+  firstName,
+  lastName,
+  isAdmin = false,
+  isUser = false,
+  email = null,
+  password = null,
+  addressLine1 = "",
+  addressLine2 = "",
+  city = "",
+  state = "",
+  zipcode = "",
+  country = "",
+  phone = null,
+  creditCard = null,
 }) {
-	console.log('getting to addUser at the back end ');
-	try {
-		if (email) {
-		const {
-			rows: [newUser],
-		} = await client.query(
-			`
+  console.log("getting to addUser at the back end ");
+  try {
+    if (email) {
+      const {
+        rows: [newUser],
+      } = await client.query(
+        `
             INSERT INTO users("isAdmin", "isUser", email, password, "firstName", "lastName", "addressLine1", "addressLine2", city, state, zipcode, country, phone, "creditCard")
             VALUES ($1, $2 ,$3 ,$4 ,$5 ,$6 ,$7 ,$8 ,$9 ,$10 ,$11 ,$12 ,$13, $14)
             ON CONFLICT DO NOTHING
             RETURNING *;
         `,
-			[
-				isAdmin,
-				isUser,
-				email,
-				password,
-				firstName,
-				lastName,
-				addressLine1,
-				addressLine2,
-				city,
-				state,
-				zipcode,
-				country,
-				phone,
-				creditCard,
-			],
-		);
+        [
+          isAdmin,
+          isUser,
+          email,
+          password,
+          firstName,
+          lastName,
+          addressLine1,
+          addressLine2,
+          city,
+          state,
+          zipcode,
+          country,
+          phone,
+          creditCard,
+        ]
+      );
 
-		if (newUser) {
-		return newUser;
-		} else {
-			return {message: 'email, phone, or credit card already exists'};			//See if need to change later
-		}
-	} else {
-		return {message: 'must enter unique email'}
-	}
-	} catch (error) {
-		throw error;
-	}
+      if (newUser) {
+        return newUser;
+      } else {
+        return { message: "email, phone, or credit card already exists" }; //See if need to change later
+      }
+    } else {
+      return { message: "must enter unique email" };
+    }
+  } catch (error) {
+    throw error;
+  }
 }
 
 //retrieve a user (check if admin)
 
 // retrieve all user
 async function getAllUsers() {
-	try {
-		const {
-			rows
-		} = await client.query(`
+  try {
+    const { rows } = await client.query(`
 			SELECT * FROM users;
 		`);
 
-		// console.log('all users: ', rows);
-		return rows;
-	} catch (error) {
-		throw error;
-	}
+    // console.log('all users: ', rows);
+    return rows;
+  } catch (error) {
+    throw error;
+  }
 }
 
 //patch a user
+async function updateUser({
+  id,
+  isAdmin,
+  isUser,
+  email,
+  password,
+  firstName,
+  lastName,
+  addressLine1,
+  addressLine2,
+  city,
+  state,
+  zipcode,
+  country,
+  phone,
+  creditCard,
+}) {
+  try {
+    const { rows } = await client.query(
+      `
+    	UPDATE users
+    	SET "isAdmin" = $2, "isUser" = $3, email = $4,
+    	password = $5, "firstName" = $6, "lastName" = $7,
+    	"addressLine1" = $8, "addressLine2" = $9, city = $10, state = $11,
+    	zipcode = $12, country = $13, phone = $14, "creditCard" = $15
+		WHERE id = $1
+		RETURNING *;
+    	`,
+      [
+        id,
+        isAdmin,
+        isUser,
+        email,
+        password,
+        firstName,
+        lastName,
+        addressLine1,
+        addressLine2,
+        city,
+        state,
+        zipcode,
+        country,
+        phone,
+        creditCard,
+      ]
+    );
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
 
-module.exports = { 
-	addUser,
-	getAllUsers
+module.exports = {
+  addUser,
+  getAllUsers,
+  updateUser,
 };
