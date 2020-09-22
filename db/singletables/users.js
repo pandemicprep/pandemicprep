3869f01fa327ae896a60614be08fd370c8fcd08c
Nicolas Olivares <nolivaresr@hotmail.com>
2020-09-22 08:34:33 -0400

    master + minor adjustment to update user. All working

--- db/singletables/users.js
+++ db/singletables/users.js
@@ -1,6 +1,8 @@
-const { client } = require('../client');
+/** @format */
 
-const bcrypt = require('bcrypt');
+const { client } = require("../client");
+
+const bcrypt = require("bcrypt");
 
 /**
  * Create new user by registration (first, last, email, pass minimum),
@@ -11,118 +13,133 @@ const bcrypt = require('bcrypt');
  * an object with an error message { message: 'the error' }
  */
 async function addUser({
-	firstName,
-	lastName,
-	isAdmin = false,
-	isUser = false,
-	email = null,
-	password = null,
-	addressLine1 = '',
-	addressLine2 = '',
-	city = '',
-	state = '',
-	zipcode = '',
-	country = '',
-	phone = null,
-	creditCard = null,
+    firstName,
+    lastName,
+    isAdmin = false,
+    isUser = false,
+    email = null,
+    password = null,
+    addressLine1 = "",
+    addressLine2 = "",
+    city = "",
+    state = "",
+    zipcode = "",
+    country = "",
+    phone = null,
+    creditCard = null,
 }) {
-	console.log('getting to addUser at the back end ', password);
-	const SALT_COUNT = 13;
-	let securedPassword = null;
-	let securedCreditCard = null;
+    console.log("getting to addUser at the back end ", password);
+    const SALT_COUNT = 13;
+    let securedPassword = null;
+    let securedCreditCard = null;
 
-	try {
-		if (email) {
-			if (password.length > 0) {
-				const validity = checkPassword(password);
-				if (validity.valid) {
-					securedPassword = await bcrypt.hash(password, SALT_COUNT);
-				} else {
-					return validity.message;
-				}
-			}
-			if (creditCard > 0) {
-				securedCreditCard = await bcrypt.hash('' + creditCard, SALT_COUNT);
-			}
-			console.log('last check of secured password ', securedPassword);
-			const {
-				rows: [newUser],
-			} = await client.query(
-				`
+    try {
+        if (email) {
+            if (password.length > 0) {
+                const validity = checkPassword(password);
+                if (validity.valid) {
+                    securedPassword = await bcrypt.hash(password, SALT_COUNT);
+                } else {
+                    return validity.message;
+                }
+            }
+            if (creditCard > 0) {
+                securedCreditCard = await bcrypt.hash("" + creditCard, SALT_COUNT);
+            }
+            console.log("last check of secured password ", securedPassword);
+            const {
+                rows: [newUser],
+            } = await client.query(
+                `
             INSERT INTO users("isAdmin", "isUser", email, password, "firstName", "lastName", "addressLine1", "addressLine2", city, state, zipcode, country, phone, "creditCard")
             VALUES ($1, $2 ,$3 ,$4 ,$5 ,$6 ,$7 ,$8 ,$9 ,$10 ,$11 ,$12 ,$13, $14)
             ON CONFLICT DO NOTHING
             RETURNING *;
         `,
 
-				[
-					isAdmin,
-					isUser,
-					email,
-					securedPassword,
-					firstName,
-					lastName,
-					addressLine1,
-					addressLine2,
-					city,
-					state,
-					zipcode,
-					country,
-					phone,
-					securedCreditCard,
-				],
-			);
+                [
+                    isAdmin,
+                    isUser,
+                    email,
+                    securedPassword,
+                    firstName,
+                    lastName,
+                    addressLine1,
+                    addressLine2,
+                    city,
+                    state,
+                    zipcode,
+                    country,
+                    phone,
+                    securedCreditCard,
+                ]
+            );
 
-			if (newUser) {
-				return newUser;
-			} else {
-				return { message: 'email or credit card already exists' }; //See if need to change later
-			}
-		} else {
-			return { message: 'must enter unique email' };
-		}
-	} catch (error) {
-		throw error;
-	}
+            if (newUser) {
+                return newUser;
+            } else {
+                return { message: "email or credit card already exists" }; //See if need to change later
+            }
+        } else {
+            return { message: "must enter unique email" };
+        }
+    } catch (error) {
+        throw error;
+    }
 }
 
 //retrieve a user (check if admin)
 
 // retrieve all user
 async function getAllUsers() {
-	try {
-		const { rows } = await client.query(`
+    try {
+        const { rows } = await client.query(`
 			SELECT * FROM users;
 		`);
 
-		// console.log('all users: ', rows);
-		return rows;
-	} catch (error) {
-		throw error;
-	}
+        // console.log('all users: ', rows);
+        return rows;
+    } catch (error) {
+        throw error;
+    }
 }
 
 //patch a user
 async function updateUser({
-	id,
-	isAdmin,
-	isUser,
-	email,
-	password,
-	firstName,
-	lastName,
-	addressLine1,
-	addressLine2,
-	city,
-	state,
-	zipcode,
-	country,
-	phone,
-	creditCard,
+    id,
+    isAdmin,
+    isUser,
+    email,
+    password,
+    firstName,
+    lastName,
+    addressLine1,
+    addressLine2,
+    city,
+    state,
+    zipcode,
+    country,
+    phone,
+    creditCard,
 }) {
-	try {
-		const { rows } = await client.query(
-			`
+    const SALT_COUNT = 13;
+    let securedPassword = null;
+    let securedCreditCard = null;
+    if (password.length > 0) {
+        const validity = checkPassword(password);
+        if (validity.valid) {
+            securedPassword = await bcrypt.hash(password, SALT_COUNT);
+        } else {
+            return validity.message;
+        }
+    }
+
+    if (creditCard > 0) {
+        securedCreditCard = await bcrypt.hash("" + creditCard, SALT_COUNT);
+    }
+    try {
+        const { rows } = await client.query(
+            `
     	UPDATE users
     	SET "isAdmin" = $2, "isUser" = $3, email = $4,
     	password = $5, "firstName" = $6, "lastName" = $7,
@@ -131,77 +148,77 @@ async function updateUser({
 		WHERE id = $1
 		RETURNING *;
     	`,
-			[
-				id,
-				isAdmin,
-				isUser,
-				email,
-				password,
-				firstName,
-				lastName,
-				addressLine1,
-				addressLine2,
-				city,
-				state,
-				zipcode,
-				country,
-				phone,
-				creditCard,
-			],
-		);
-		return rows;
-	} catch (error) {
-		throw error;
-	}
+            [
+                id,
+                isAdmin,
+                isUser,
+                email,
+                securedPassword,
+                firstName,
+                lastName,
+                addressLine1,
+                addressLine2,
+                city,
+                state,
+                zipcode,
+                country,
+                phone,
+                securedCreditCard,
+            ]
+        );
+        return rows;
+    } catch (error) {
+        throw error;
+    }
 }
 
 async function getUserById(id) {
-	try {
-		const {
-			rows: [user],
-		} = await client.query(
-			`
+    try {
+        const {
+            rows: [user],
+        } = await client.query(
+            `
 		SELECT * FROM users
 		WHERE id = $1;`,
-			[id],
-		);
-		return user;
-	} catch (error) {
-		throw error;
-	}
+            [id]
+        );
+        return user;
+    } catch (error) {
+        throw error;
+    }
 }
 
 module.exports = {
-	addUser,
-	getAllUsers,
-	updateUser,
-	getUserById,
+    addUser,
+    getAllUsers,
+    updateUser,
+    getUserById,
 };
 
 //Helper functions
 function checkPassword(password) {
-	if (password.length < 8) {
-		return { valid: false, message: 'This password is too short. 8 characters or more' };
-	}
-	const passwordCharacters = password.split('');
-	let hasNumber = false;
-	let hasUpper = false;
-	passwordCharacters.forEach((char) => {
-		if (char >= 0 && char <= 10) {
-			hasNumber = true;
-		}
-		if (char >= 'A' && char <= 'Z') {
-			hasUpper = true;
-		}
-	});
-	if (hasNumber) {
-		return { valid: true, message: 'valid' };
-	} else {
-		return { valid: false, message: 'Password must have at least one number' };
-	}
-	if (hasUpper) {
-		return { valid: true, message: 'valid' };
-	} else {
-		return { valid: false, message: 'Password must have at least one uppercase letter' };
-	}
+    if (password.length < 8) {
+        return { valid: false, message: "This password is too short. 8 characters or more" };
+    }
+    const passwordCharacters = password.split("");
+    let hasNumber = false;
+    let hasUpper = false;
+    passwordCharacters.forEach((char) => {
+        if (char >= 0 && char <= 10) {
+            hasNumber = true;
+        }
+        if (char >= "A" && char <= "Z") {
+            hasUpper = true;
+        }
+    });
+    if (hasNumber) {
+        return { valid: true, message: "valid" };
+    } else {
+        return { valid: false, message: "Password must have at least one number" };
+    }
+    if (hasUpper) {
+        return { valid: true, message: "valid" };
+    } else {
+        return { valid: false, message: "Password must have at least one uppercase letter" };
+    }
 }
