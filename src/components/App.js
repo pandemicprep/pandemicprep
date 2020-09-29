0e9a1f1f03920fc2513cb4aa4babc525aa1061dc
Falon Landers <falonlanders@outlook.com>
2020-09-28 20:08:32 -0400

    altered the header and footer

--- src/components/App.js
+++ src/components/App.js
@@ -29,96 +29,79 @@ import { getUserFromToken } from '../api/users';
 import './App.css';
 
 const App = () => {
-  	const [user, setUser] = useState({ firstName: 'Guest', isAdmin: false, isUser: false, token: '' });
-    const [products, setProducts] = useState([]);
-    const [promotedProducts, setPromotedProducts] = useState([]);
-    const [product, setProduct] = useState({});
-    const [searchObject, setSearchObject] = useState('');
-    const [category, setCategory] = useState('');    // const history = useHistory();
-    const [pageType, setPageType] = useState('');
-  	const [view, setView] = useState('');
+	const [user, setUser] = useState({ firstName: 'Guest', isAdmin: false, isUser: false, token: '' });
+	const [products, setProducts] = useState([]);
+	const [promotedProducts, setPromotedProducts] = useState([]);
+	const [product, setProduct] = useState({});
+	const [searchObject, setSearchObject] = useState('');
+	const [category, setCategory] = useState('');    // const history = useHistory();
+	const [pageType, setPageType] = useState('');
+	const [view, setView] = useState('');
 
 
 	useEffect(() => {
 		getPromotedProducts()
-            .then((response) => {
-                setPromotedProducts(response)
-                setProducts(response)
-            })
-            .catch((error) => {
-                console.error(error)
-            });
+			.then((response) => {
+				setPromotedProducts(response)
+				setProducts(response)
+			})
+			.catch((error) => {
+				console.error(error)
+			});
 		if (localStorage.getItem('panprepToken')) {
 			getUserFromToken(localStorage.getItem('panprepToken'))
 				.then((response) => {
-                    console.log('the updated user on load is ', response);
+					console.log('the updated user on load is ', response);
 					setUser(response);
-					
+
 				})
 				.catch((error) => {
 					console.error(error);
 				});
-        }
-       
-	}, []);
+		}
 
+	}, []);
 
 	return (
 		<Router>
-			<div className='App'>
-				<Header
-					products={products}
-					setProducts={setProducts}
-					searchString={searchString}
-					setSearchString={setSearchString}
-					useHistory={useHistory}
-					NavLink={NavLink}
-					setView={setView}
-    promotedProducts={promotedProducts}
-				/>
+			<div className="App">
+				<Header products={products} setProducts={setProducts}
+					searchObject={searchObject} setSearchObject={setSearchObject} useHistory={useHistory}
+					NavLink={NavLink} promotedProducts={promotedProducts} setPageType={setPageType} />
 				<Switch>
-					<Route exact path='/'>
-						<Productlist
-							products={products}
-							setProducts={setProducts}
-							setProduct={setProduct}
-							NavLink={NavLink}
-    searchString={searchString} category={category}
-						/>
-						<Categories setProducts={setProducts} NavLink={NavLink} setCategory={setCategory}/>
+					<Route exact path='/' >
+						<Productlist products={products} setProducts={setProducts} setProduct={setProduct} NavLink={NavLink}
+							searchObject={searchObject} category={category} pageType={pageType} setPageType={setPageType} />
+						<Categories setProducts={setProducts} NavLink={NavLink} setCategory={setCategory} setPageType={setPageType} />
 					</Route>
 					<Route path='/register'>
-						<Profile view={view} setUser={setUser} useHistory={useHistory} />
+						<Profile view='register' />
 					</Route>
-					<Route path='/login'>
-						<Profile view={view} setUser={setUser} useHistory={useHistory} />
+					<Route path='/login' render={() =>
+						<Profile view='login' />
+					} >
 					</Route>
 					<Route path='/guest'>
-						<Profile view={view} useHistory={useHistory} />
+						<Profile view='guest' />
 					</Route>
-					<Route path='/edit-user' setUser={setUser}>
-						<Profile
-							view={view}
-							setView={setView}
-							user={user}
-							useHistory={useHistory}
-						/>
+					<Route path='/edit-user'>
+						<Profile view='edit' />
 					</Route>
-					<Route path='/product'>
+					<Route path='/product' >
 						<Product product={product} setProduct={setProduct} />
-						<Categories setProducts={setProducts} NavLink={NavLink} setCategory={setCategory}/>
+						<Categories setProducts={setProducts} NavLink={NavLink} setCategory={setCategory} setPageType={setPageType} />
 					</Route>
-					<Route path='/cart'>
-						<Cart user={user} />
+					<Route path='/cart' >
+						<Cart />
 					</Route>
-					<Route path='/orders'>
-						<Orders user={user} />
+					<Route path='/orders' >
+						<Orders />
 					</Route>
-					<Route path='/user-list'>
-						<Userlist user={user} />
+					<Route path='/user-list' >
+						<Userlist />
 					</Route>
-					<Route path='/sales'>
-						<Sales user={user} />
+					<Route path='/sales' >
+						<Sales />
 					</Route>
 					<Redirect to='/' />
 				</Switch>
@@ -126,54 +109,6 @@ const App = () => {
 			</div>
 		</Router>
 	);
-
-    return (
-        <Router>
-        <div className="App">
-            <Header products={products} setProducts={setProducts}
-            searchObject={searchObject} setSearchObject={setSearchObject} useHistory={useHistory} 
-            NavLink={NavLink} promotedProducts={promotedProducts} setPageType={setPageType} />
-            <Switch>
-                <Route exact path='/' >
-                        <Productlist products={products} setProducts={setProducts} setProduct={setProduct} NavLink={NavLink} 
-                        searchObject={searchObject} category={category} pageType={pageType} setPageType={setPageType} />
-                        <Categories setProducts={setProducts} NavLink={NavLink} setCategory={setCategory} setPageType={setPageType}/>
-                    </Route>
-                    <Route path='/register'>
-                        <Profile view='register'/>
-                    </Route>
-                    <Route path='/login' render={() => 
-                        <Profile view='login' />
-                    } >
-                    </Route>
-                    <Route path='/guest'>
-                        <Profile view='guest' />
-                    </Route>
-                    <Route path='/edit-user'>
-                        <Profile view='edit' />
-                    </Route>
-                    <Route path='/product' >
-                        <Product product={product} setProduct={setProduct} />
-                        <Categories setProducts={setProducts} NavLink={NavLink} setCategory={setCategory} setPageType={setPageType}/>
-                    </Route>
-                    <Route path='/cart' >
-                        <Cart />
-                    </Route>
-                    <Route path='/orders' >
-                        <Orders />
-                    </Route>
-                    <Route path='/user-list' >
-                        <Userlist />
-                    </Route>
-                    <Route path='/sales' >
-                        <Sales />
-                    </Route>
-                    <Redirect to='/' />
-                </Switch>
-            <Footer />
-        </div>
-        </Router>
-    );
 
 };
 
--- src/components/pages/footer/Footer.js
+++ src/components/pages/footer/Footer.js
@@ -7,7 +7,11 @@ export const Footer = () => {
     return (
         <footer>
             <div className="footerDiv">
-                <h2>Footer Stuff Here....</h2>
+                <h2>
+                    <img id="dooms" src={process.env.PUBLIC_URL + '/styleimages/dooms.png'} />
+                    &#169; Doomsday Preppers
+                <img id="disaster" src={process.env.PUBLIC_URL + '/styleimages/disaster.png'} />
+                </h2>
             </div>
         </footer>
     )
