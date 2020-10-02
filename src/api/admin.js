3311a7989c4a5f20c8ac299b569198f03a17fbfb
Matthew Forgette <mj0723@gmail.com>
2020-10-02 19:47:06 -0400

    finished the patch for products in admin tab

--- src/api/admin.js
+++ src/api/admin.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 
 export async function getAllUsers(pageNumber) {
     try {
-      const { data } = await axios.get(`/api/users/${pageNumber}`);
+      const { data } = await axios.get(`/api/admin/users/${pageNumber}`);
       console.log('all users in src api index: ', data);
       return data;
     } catch (error) {
@@ -15,9 +15,9 @@ export async function getAllUsers(pageNumber) {
     }
 }
 
-export async function getAllProducts(pageNumber) {
+export async function getAllProducts(pageNumber, token) {
   try {
-    const { data } = await axios.get(`/api/admin/${pageNumber}`);
+    const { data } = await axios.get(`/api/admin/products/${pageNumber}`, {headers: {Authorization: 'Bearer ' + token}});
 
     return data;
   } catch (error) {
@@ -25,9 +25,10 @@ export async function getAllProducts(pageNumber) {
   }
 }
 
-export async function updateProduct(id, fields = {}) {
+export async function updateProduct({id, fields, token}) {
   try {
-    const { data } = await axios.patch('/', id, fields);
+    console.log('getting to axios call', token)
+    const { data } = await axios.patch('/api/admin', {id, fields}, {headers: {Authorization: 'Bearer ' + token}});
 
     return data;
   } catch (error) {
