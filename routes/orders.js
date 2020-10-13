32b7d4fe8b4b12e08ac8e5c661aec2ccd76d4db5
Nicolas Olivares <nolivaresr@hotmail.com>
2020-10-12 22:29:29 -0400

    checkout almost done

--- routes/orders.js
+++ routes/orders.js
@@ -36,11 +36,20 @@ ordersRouter.post('/', async (req, res, next) => {
 
 ordersRouter.post('/create-checkout-session', async (req, res) => {
    
-  const {cart} = req.body;
+  const {cart} = req.body
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
       line_items: [
-        cart
+        {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: 'Total',
+            },
+            unit_amount: parseInt((cart.total * 100) + 500),
+          },
+          quantity: 1,
+        },
       ],
       mode: 'payment',
       success_url: 'http://localhost:3000/success',
@@ -52,3 +61,24 @@ ordersRouter.post('/create-checkout-session', async (req, res) => {
   });
 
 module.exports = ordersRouter;
+
+
+// price: {
+//   'id': 1,
+//   'object': "price",
+//   'active': true,
+//   'billing_scheme': 'per_unit',
+//   'created': 1599082103,
+//   'currency': 'usd',
+//   'livemode': false,
+//   'lookup_key': null,
+//   'metadata': {},
+//   'nickname': null,
+//   'product': 'pencils',
+//   'recurring': null,
+//   'tiers_mode': null,
+//   'transform_quantity': null,
+//   'type': 'one_time',
+//   'unit_amount': 9,
+//   'unit_amount_decimal': 99
+// },
